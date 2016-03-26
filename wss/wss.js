814b2dbd5290443fa86e47ac372b0ff63fd1f2e9
Runbiao Zhong <timzhong2374@hotmail.co.uk>
2016-03-26 19:19:22 -0400

    test

--- wss/wss.js
+++ wss/wss.js
@@ -52,7 +52,7 @@ function register(socket,username,roomname){
 }
 
 function processasobj(s){
-	var a = '"userid":"u56f717dc21a3a","name":"test","lat":"43.665188699999995","lng":"-79.3891484","cmd":"addmarker"';
+	var a = '{"userid":"u56f717dc21a3a","name":"test","lat":"43.665188699999995","lng":"-79.3891484","cmd":"addmarker"}';
 	var obj = JSON.parse(a);
 	if(obj.cmd == 'addmarker'){
 		console.log('userid: '+obj.userid);
