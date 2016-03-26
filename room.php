6a1546bef56f2044e1c19e0acd88933f6fe61cef
Runbiao Zhong <timzhong2374@hotmail.co.uk>
2016-03-26 12:16:47 -0400

    room.php

--- room.php
+++ room.php
@@ -1,5 +1,31 @@
 <?php 
-include "icl/findusers.inc.php";
+include "connect.php";
+
+
+function findusers(){
+	$roomname = $_GET['room'];
+	
+	global $db;
+
+	$query = "select * from users where roomid = (select roomid from rooms where roomname = '".$roomname."' limit 1)";
+	$rs = sql_query($query,$db);
+
+	$jsonstr = "[";
+	while($user = sql_fetch_array($rs)){
+		
+
+		$username = $user['username'];
+		$lat = $user['latitude'];
+		$lng = $user['longitude'];
+
+		$jsonstr .= "[";
+		$jsonstr .= "'".$username."',$lat,$lng";
+		$jsonstr .= "],";
+	}
+	$jsonstr .= "]";
+	return $jsonstr;
+
+}
 
 ?>
 <html lang="en">
