edff241b46313f4094c498d0d932de5679d9cc23
Runbiao Zhong <timzhong2374@hotmail.co.uk>
2016-03-26 19:53:07 -0400

    getCookie

--- room.php
+++ room.php
@@ -116,11 +116,21 @@ function findusers(){
 
 	var socket = null;
 	var url = "ws://ec2-52-37-132-185.us-west-2.compute.amazonaws.com:9797";
-	var username = $_COOKIE['user'];
+	var username = getCookie('user');
 	var roomname = '<?php echo $_GET['room']; ?>';
 	connect(socket,url,username,roomname);
 	
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
 // ==================================================== //
 
 	var markers = <?php echo findusers();?>;
